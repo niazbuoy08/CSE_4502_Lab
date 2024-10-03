c75cf472e00a9fa78e44a7c48d942755feea7877
niazbuoy08 <rahmanniaz29@gmail.com>
2024-10-03 23:38:31 +0600

    Second commit

--- mcertikos/kern/vmm/MPTIntro/MPTIntro.c
+++ mcertikos/kern/vmm/MPTIntro/MPTIntro.c
@@ -4,102 +4,120 @@
 
 #include "import.h"
 
-#define PT_PERM_UP  0
+#define PT_PERM_UP 0
 #define PT_PERM_PTU (PTE_P | PTE_W | PTE_U)
 
 /**
  * Page directory pool for NUM_IDS processes.
  * mCertiKOS maintains one page structure for each process.
- * Each PDirPool[index] represents the page directory of the page structure
- * for the process # [index].
- * In mCertiKOS, we statically allocate page directories, and maintain the second
- * level page tables dynamically.
- * The unsigned int * type is meant to suggest that the contents of the array
- * are pointers to page tables. In reality they are actually page directory
- * entries, which are essentially pointers plus permission bits. The functions
- * in this layer will require casting between integers and pointers anyway and
- * in fact any 32-bit type is fine, so feel free to change it if it makes more
- * sense to you with a different type.
+ * Each PDirPool[index] represents the page directory of the page structure for the process # [index].
+ * In mCertiKOS, we statically allocate page directories, and maintain the second level page tables dynamically.
  */
-unsigned int *PDirPool[NUM_IDS][1024] gcc_aligned(PAGESIZE);
+char * PDirPool[NUM_IDS][1024] gcc_aligned(PAGESIZE);
 
 /**
  * In mCertiKOS, we use identity page table mappings for the kernel memory.
- * IDPTbl is an array of statically allocated identity page tables that will be
- * reused for all the kernel memory.
- * That is, in every page directory, the entries that fall into the range of
- * addresses reserved for the kernel will point to an entry in IDPTbl.
+ * IDPTbl is a statically allocated, identity page table that will be reused for
+ * all the kernel memory.
+ * Every page directory entry of kernel memory links to corresponding entry in IDPTbl.
  */
 unsigned int IDPTbl[1024][1024] gcc_aligned(PAGESIZE);
 
-// Sets the CR3 register with the start address of the page structure for process # [index].
+
+// sets the CR3 register with the start address of the page structure for process # [index]
 void set_pdir_base(unsigned int index)
 {
     // TODO
+    set_cr3(PDirPool[index]);
 }
 
-// Returns the page directory entry # [pde_index] of the process # [proc_index].
-// This can be used to test whether the page directory entry is mapped.
+// returns the page directory entry # [pde_index] of the process # [proc_index]
+// this can be used to test whether the page directory entry is mapped
 unsigned int get_pdir_entry(unsigned int proc_index, unsigned int pde_index)
 {
-    // TODO
-    return 0;
+    // TODO??? return pagetable base address of entire
+    // TODO TODO TODO
+    return (unsigned int)PDirPool[proc_index][pde_index];
 }
 
-// Sets the specified page directory entry with the start address of physical
-// page # [page_index].
-// You should also set the permissions PTE_P, PTE_W, and PTE_U.
-void set_pdir_entry(unsigned int proc_index, unsigned int pde_index,
-                    unsigned int page_index)
+// sets specified page directory entry with the start address of physical page # [page_index].
+// you should also set the permissions PTE_P, PTE_W, and PTE_U
+void set_pdir_entry(unsigned int proc_index, unsigned int pde_index, unsigned int page_index)
 {
     // TODO
+    // bit 31-12 is the base address of page table
+    unsigned int value = (page_index << 12) | PT_PERM_PTU;
+    PDirPool[proc_index][pde_index] = (char *)value;
+
 }
 
-// Sets the page directory entry # [pde_index] for the process # [proc_index]
-// with the initial address of page directory # [pde_index] in IDPTbl.
-// You should also set the permissions PTE_P, PTE_W, and PTE_U.
-// This will be used to map a page directory entry to an identity page table.
+// sets the page directory entry # [pde_index] for the process # [proc_index]
+// with the initial address of page directory # [pde_index] in IDPTbl
+// you should also set the permissions PTE_P, PTE_W, and PTE_U
+// this will be used to map the page directory entry to identity page table.
 void set_pdir_entry_identity(unsigned int proc_index, unsigned int pde_index)
 {
     // TODO
+    // the address of IDPTbl[pde_index] is aligned to 4096
+    // so the low 12bits are all 0
+    unsigned int value = (unsigned int)IDPTbl[pde_index];
+    value |= PT_PERM_PTU;
+    PDirPool[proc_index][pde_index] = (char *)value;
 }
 
-// Removes the specified page directory entry (sets the page directory entry to 0).
-// Don't forget to cast the value to (unsigned int *).
+// removes specified page directory entry (set the page directory entry to 0).
+// don't forget to cast the value to (char *).
 void rmv_pdir_entry(unsigned int proc_index, unsigned int pde_index)
 {
     // TODO
+    PDirPool[proc_index][pde_index] = (char *)0x00000000;
 }
 
-// Returns the specified page table entry.
-// Do not forget that the permission info is also stored in the page directory entries.
-unsigned int get_ptbl_entry(unsigned int proc_index, unsigned int pde_index,
-                            unsigned int pte_index)
-{
-    // TODO
-    return 0;
+// returns the specified page table entry.
+// do not forget that the permission info is also stored in the page directory entries.
+unsigned int get_ptbl_entry(unsigned int proc_index, unsigned int pde_index, unsigned int pte_index)
+{   // PdirPool[proc_index][pde_index] stores the bass address of a page table
+    // pte_index * 4 means each page table entry is 4 byte.
+    unsigned int pte_addr = (unsigned int )PDirPool[proc_index][pde_index];
+    pte_addr &= 0xfffff000; //remove perm bits
+    pte_addr += pte_index << 2;//
+    return *(unsigned int *)pte_addr;
 }
 
-// Sets the specified page table entry with the start address of physical page # [page_index]
-// You should also set the given permission.
-void set_ptbl_entry(unsigned int proc_index, unsigned int pde_index,
-                    unsigned int pte_index, unsigned int page_index,
-                    unsigned int perm)
+// sets specified page table entry with the start address of physical page # [page_index]
+// you should also set the given permission
+void set_ptbl_entry(unsigned int proc_index, unsigned int pde_index, unsigned int pte_index, unsigned int page_index, unsigned int perm)
 {
     // TODO
+    // each page table entry is 4 byte, char * is also 4 byte
+    unsigned int* pte;
+    unsigned int pte_addr =  (unsigned int )PDirPool[proc_index][pde_index];
+    pte_addr &= 0xfffff000;//rmove perm bits
+    pte_addr += pte_index << 2;
+
+    pte = (unsigned int *)pte_addr;
+    *pte &= 0x00000000;
+    *pte = page_index << 12;
+    *pte |= (perm & 0x00000fff);
 }
 
-// Sets up the specified page table entry in IDPTbl as the identity map.
-// You should also set the given permission.
-void set_ptbl_entry_identity(unsigned int pde_index, unsigned int pte_index,
-                             unsigned int perm)
+// sets the specified page table entry in IDPTbl as the identity map.
+// you should also set the given permission
+void set_ptbl_entry_identity(unsigned int pde_index, unsigned int pte_index, unsigned int perm)
 {
     // TODO
+    IDPTbl[pde_index][pte_index] = ((pde_index << 10) + pte_index) << 12;
+    IDPTbl[pde_index][pte_index] |= perm;
 }
 
-// Sets the specified page table entry to 0.
-void rmv_ptbl_entry(unsigned int proc_index, unsigned int pde_index,
-                    unsigned int pte_index)
+// sets the specified page table entry to 0
+void rmv_ptbl_entry(unsigned int proc_index, unsigned int pde_index, unsigned int pte_index)
 {
     // TODO
+    unsigned int * pte;
+    unsigned int pte_addr = (unsigned int)PDirPool[proc_index][pde_index];
+    pte_addr &= 0xfffff000;//remove perm bits
+    pte_addr += pte_index << 2;
+    pte = (unsigned int *)pte_addr;
+    *pte &= 0x00000000;
 }
